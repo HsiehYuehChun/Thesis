f6f89abaf95b88d9d8d96a82f0dfa31931d17e97
Hsieh Yueh-Chen <wilson5711704@gmail.com>
2025-06-19 15:45:40 +0800

    Update JDcalculater.m

--- JDcalculater.m
+++ JDcalculater.m
@@ -10,6 +10,8 @@ load('second.mat')
 FN = 1352;
 
 for sample = 1:FN
+    % 使用 MATLAB 內建的 juliandate 函數進行轉換
     JD{sample}(:) = juliandate( [ year{sample}(:) , month{sample}(:) , date{sample}(:) , hour{sample}(:) , minute{sample}(:) , second{sample}(:) ] );
 end
+% 將轉換後的儒略日結果儲存到 JD.mat 檔案，使用 -v7.3 格式以支援大型資料
 save('JD.mat', 'JD','-v7.3');
